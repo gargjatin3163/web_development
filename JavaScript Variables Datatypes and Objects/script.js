c1ce3b9f82c001ca1a4bd4c86585699e4029c326
Jatin garg <gargjatin3163@gmail.com>
2024-06-01 17:41:56 +0530

    Web development

---
+++ JavaScript Variables Datatypes and Objects/script.js
@@ -0,0 +1,28 @@
+console.log("Hey! Hello world")
+
+var a=8;
+var b=5;
+var c="Jatin";
+
+console.log(a+b+8)
+console.log(typeof a,typeof b,typeof c)
+
+let x="Hello world";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o={
+    name:"Jatin",
+    age:22
+}
+
+console.log(o)
+
+o.salary="100 crores"
+console.log(o)
